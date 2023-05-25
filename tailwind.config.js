6dd390afc5594185bcdd69293b83a91163fcc176
kodegoCJ <gambalacharles96gmail.com>
2023-05-25 18:02:00 +0800

    modified:   composer.lock 	modified:   package-lock.json 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-      "./node_modules/flowbite/**/*.js"
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue"
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-     require('flowbite/plugin')
-  ],
+  plugins: [],
 }
 
